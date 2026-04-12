a487a2c90d91f5a1f71d570f93c0a5be1214d331
Jorge Vargas Lara <jorgeblue5210@gmail.com>
2026-04-12 04:45:34 -0700

    fixed astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import preact from '@astrojs/preact';
-
 import mdx from '@astrojs/mdx';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), mdx()]
+  site: 'https://jorge-l-vargas.netlify.app',
+  integrations: [preact(), mdx()],
 });
